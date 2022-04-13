d2b27c9399c4af65b467747f38637fd4bbcc289c
Drake Horne <drakehorne@Drakes-MacBook-Air.local>
2022-04-13 09:04:09 -0700

    Registered car model with admin site + completed post functionality for car + updated Rest Framework.settings

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,23 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CarSerializer
+from .models import Car
+from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def cars_list(request):
 
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
 
-    return Response('ok')
+    elif request.method == 'POST':
+        serializer = CarSerializer(data=request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)       
+
+
+            
+
